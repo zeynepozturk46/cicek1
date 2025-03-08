6e026d2948b273cb5f8cca8a2a7eaa7772c1c180
zeynepozturk46 <zeynepozturk46@icloud.com>
2025-03-08 15:43:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.getElementById("message").style.opacity = "1";
+    }, 5000); // 5 saniye sonra mesaj belirecek
+});
